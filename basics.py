9a8ddc4f5c84774ebdd29559c7cfd46c8d26c734
unknown <sameeracnair283@gmail.com>
2026-04-14 12:53:46 +0530

    py commit

---
+++ basics.py
@@ -0,0 +1,31 @@
+a="sameera"
+print(a)
+a=5
+print(a)
+a=16.3
+print(a)
+b=[1,2,3,4,5]
+print(b)
+print(b[0])
+print(b[4])
+print(b[-1])
+b.append(6)
+print(b)
+b.insert(2,7)
+print(b)
+b.pop(b[4])
+print(b)
+b.pop(4)
+print(b)
+
+
+
+# tuple
+t=("apple","banana","grapes")
+print(t)
+l =list(t)
+print(l)
+l.append("cherry")
+print(l)
+t=tuple(l)
+print(t)
---
+++ conditions.py
@@ -0,0 +1,174 @@
+age=18
+if age>=18:
+  print("eligible to vote")
+else:
+  print("not eligible to vote")
+
+# a=int(input("enter a number"))
+# if (a%2==0):
+#   print(a, "is even")
+# else:
+#   print(a ,"is odd")
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input ("enter a number"))
+# if (a>b)and(a>c):
+#     print(a,"is greater")
+# elif (b>c):
+#     print(b," is greater")
+# else:
+#     print(c,"is greater")
+# num = float(input("Enter a number: "))
+
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+
+# num=int(input("enter a numbe"))
+# if (num%2==0):
+#     print(num,"is even")
+# else:
+#     print(num,"is odd")
+
+
+
+# age=int(input("enter age:"))
+# if(age>=18):
+#   print("eligible to vote")
+# else:
+#   print("not eligible to vote")
+
+# number=int(input("enter a number:"))
+# if number%5==0:
+#   print(number,"is divisible by 5")
+# else:
+#   print(number ,"is not divisible by 5")
+
+
+# char=input("enter a charcter:")
+# if char.lower() in 'aeiou':
+#   print(char,"is vowel")
+# else:
+#   print(char,"is consonent")
+
+
+
+#   number = float(input("Enter a number: "))
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number < 0:
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
+
+
+# num=21
+# if num%3==0 and num%7==0:
+#  print(num,"is a multiple of 3 and 7")
+
+
+# pasword="secret@1828"
+
+# conformpasswrd = input("enter a password")
+# if pasword==conformpasswrd:
+#   print("conformed")
+# else:
+#   print("not conformed")
+
+
+# num1=float(input("enter first number"))
+# num2=float(input("enter second number"))
+
+# print("1.Add(+),2.sub(-),3.mul(*),4.div(/)")
+# choice=(input("enter your choice (1/2/3/4):"))
+
+# if choice=='1':
+#   print(f"result ={num1+num2}")
+# elif choice=='2':
+#   print(f"result = {num1-num2}")
+# elif choice=='3':
+#   print(f"result = {num1*num2}")
+# elif choice=='4':
+
+#   if num2!=0:
+#    print(f"result = {num1/num2}")
+#   else:
+#    print("error! division by 0")
+# else:
+#   print("invalid input")
+
+
+# year=int(input("enter  a year"))
+# if (year % 400 == 0) or (year % 4==0 and year % 100!=0):
+#   print(year,"is leap year")
+# else:
+#   print(year,"is not a leap year")
+
+# num=int(input("enter a number"))
+# if num%2==0 and num%5==0:
+#   print(num,"is divisible by both 2 and 5")
+# else:
+#   print(num,"is not divisible by 2 and 5")
+
+
+# x=int(input("enter a number"))
+# if 1<=x<=100:
+#   print(x,"lies between 1 and 100")
+# else:
+#   print(x,"not lies between 1 and 100")
+
+
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# c=int(input("enter third number"))
+
+# if a<=b and a<=c:
+#   print(a,"is smallest")
+# elif b<c:
+#   print(b,"is smallest")
+# else:
+#   print(c,"is smallest")
+
+
+# balance=1000
+# print(balance,"is your current balance")
+# withdraw_amount=float(input("enter number of withdrawel"))
+
+# if withdraw_amount<=0:
+#   print("error:please a positive number")
+# elif withdraw_amount<=balance:
+#   balance -= withdraw_amount
+#   print("withdrawel successfull!balance:",balance)
+# else:
+#   print("transaction failed!insufficient balance")
+
+age=int(input("enter your age"))
+if age<12:
+  price=100
+  print("category:child")
+elif age<=60:
+  price=200
+  print("category:Adult")
+else:
+  price=150
+  print("category:senior citizen")
+print(f"ticket price:{price}")
+
+
+
+
+  
+
+
+
+
+
+
+
+
